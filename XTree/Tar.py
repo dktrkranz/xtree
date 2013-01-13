7faff5c2a1c94350c670112bacfe921bc0f8b463
Luca Falavigna <dktrkranz@debian.org>
2013-01-13 15:47:07 +0100

    Add 2013 to copyright years

--- XTree/Tar.py
+++ XTree/Tar.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Luca Falavigna
+# Copyright (C) 2012-2013 Luca Falavigna
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
